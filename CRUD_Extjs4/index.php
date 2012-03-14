6bd3b87be6b8dadde2aa7a51c526ec7972e79ab8
ErnestoR <ernesto.ramirez88@gmail.com>
2012-03-13 23:11:46 -0600

    Changed to ext-all-debug

--- CRUD_Extjs4/index.php
+++ CRUD_Extjs4/index.php
@@ -5,7 +5,7 @@
 
     <link rel="stylesheet" type="text/css" href="extjs/resources/css/ext-all.css">
     
-    <script type="text/javascript" src="extjs/ext-debug.js"></script>
+    <script type="text/javascript" src="extjs/ext-all-debug.js"></script>
     <link rel="stylesheet" href="resources/style.css" type="text/css">
     <script type="text/javascript" src="app.js"></script>
 </head>
